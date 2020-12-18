2015b2bf805968b3f12365a51d18bb8946b7deb7
Manas Bondale <m_bondale@u.pacific.edu>
2020-12-18 14:53:58 +0530

    v1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require the discord.js module
 const Discord = require('discord.js')
-
+const config = require('./config.json')
 //load .env
 require('dotenv').config()
 
@@ -13,5 +13,18 @@ client.once('ready', () => {
 	console.log('Ready!')
 })
 
+client.on('message', message => {
+	if(!message.content.startsWith(config.prefix) || message.author.bot) return;
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
+	
+	console.log(message.content);
+});
 // login to Discord with your app's token
 client.login(process.env.DISCORD_BOT_TOKEN)
